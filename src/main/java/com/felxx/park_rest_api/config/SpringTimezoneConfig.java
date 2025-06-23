f2b4e7eb7081378875636fcced3953e95c172e77
felxx <igorfelipemonteirocontato@gmail.com>
2025-06-23 14:34:03 -0300

    project configured

---
+++ src/main/java/com/felxx/park_rest_api/config/SpringTimezoneConfig.java
@@ -0,0 +1,16 @@
+package com.felxx.park_rest_api.config;
+
+import java.util.TimeZone;
+
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class SpringTimezoneConfig {
+    
+    @PostConstruct
+    public void timezoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+}
