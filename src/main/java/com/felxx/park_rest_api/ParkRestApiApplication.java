3fb2be8adba67c34b8e10f454e6c58556f67d7c3
felxx <igorfelipemonteirocontato@gmail.com>
2025-06-23 14:03:50 -0300

    project created

---
+++ src/main/java/com/felxx/park_rest_api/ParkRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.felxx.park_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkRestApiApplication.class, args);
+	}
+
+}
