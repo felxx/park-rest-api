fc9659401f4532d37af54085e27ee2adc22e2d49
felxx <igorfelipemonteirocontato@gmail.com>
2025-06-24 15:51:32 -0300

    feat: implement spring documentation

---
+++ src/main/java/com/felxx/park_rest_api/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.felxx.park_rest_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+    
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Park REST API")
+                        .version("1.0.0")
+                        .description("API for managing parking system")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0"))
+                        .contact(new Contact()
+                                .name("felxx")
+                                .email("igorfelipemonteirocontato@gmail.com")));
+    }
+}
--- src/main/java/com/felxx/park_rest_api/web/controllers/UserController.java
+++ src/main/java/com/felxx/park_rest_api/web/controllers/UserController.java
@@ -15,7 +15,13 @@ import com.felxx.park_rest_api.web.dto.UserCreateDto;
 import com.felxx.park_rest_api.web.dto.UserPasswordDto;
 import com.felxx.park_rest_api.web.dto.UserResponseDto;
 import com.felxx.park_rest_api.web.dto.mapper.UserMapper;
+import com.felxx.park_rest_api.web.exceptions.ErrorMessage;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,8 +29,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-
+@Tag(name = "User Management", description = "Operations related to user management")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/users")
@@ -39,18 +44,33 @@ public class UserController {
         return ResponseEntity.ok().body(UserMapper.toListDto(users));
     }
 
+
+    @Operation(summary = "Retrieve a user by his id", responses = {
+        @ApiResponse(responseCode = "200", description = "User succesfully found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+        @ApiResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+        })
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDto> findById(@PathVariable Long id) {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(UserMapper.toDto(user));
     }
-
+    
+    @Operation(summary = "Create a new user", responses = {
+        @ApiResponse(responseCode = "201", description = "User created successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+        @ApiResponse(responseCode = "409", description = "E-mail already exists", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+        @ApiResponse(responseCode = "422", description = "Invalid input data", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+        })
     @PostMapping
     public ResponseEntity<UserResponseDto> create(@Valid @RequestBody UserCreateDto userCreateDto) {
         User newUser = userService.save(UserMapper.toUser(userCreateDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toDto(newUser));
     }
     
+    @Operation(summary = "Change a user password", responses = {
+        @ApiResponse(responseCode = "204", description = "Password changed", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid password", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+        @ApiResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+        })
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id, @Valid @RequestBody UserPasswordDto userPasswordDto) {
         userService.changePassword(id, userPasswordDto.getCurrentPassword(), userPasswordDto.getNewPassword(), userPasswordDto.getConfirmPassword());
