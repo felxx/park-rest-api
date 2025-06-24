1a8ae44c38711d47fa6547637548a5a16a69a31d
felxx <igorfelipemonteirocontato@gmail.com>
2025-06-24 14:50:21 -0300

    feat: implement error message

---
+++ src/main/java/com/felxx/park_rest_api/web/exceptions/ErrorMessage.java
@@ -0,0 +1,50 @@
+package com.felxx.park_rest_api.web.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter @ToString
+public class ErrorMessage {
+    
+    private String path;
+    private String method;
+    private int status;
+    private String statusText;
+    private String message;
+    private Map<String, Object> errors;
+
+    public ErrorMessage() {
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult bindingResult) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+        addErrors(bindingResult);
+    }
+
+    private void addErrors(BindingResult bindingResult) {
+        this.errors = new HashMap<>();
+        for(FieldError fieldError : bindingResult.getFieldErrors()) {
+            this.errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
